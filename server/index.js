cbfeeba2c54cb3b062dfe76d200586ce2574ee13
cixiu <cheng597604817@qq.com>
2018-04-02 17:43:08 +0800

    优化生产服务启动

--- server/index.js
+++ server/index.js
@@ -11,17 +11,20 @@ const app = new Koa()
 
 // 设置CORS
 app.use(async (ctx, next) => {
-	ctx.set("Access-Control-Allow-Origin", ctx.headers.origin || '*');
-	ctx.set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-	ctx.set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  ctx.set("Access-Control-Allow-Credentials", true); //可以带cookies
-	ctx.set("X-Powered-By", '3.2.1')
-	if (ctx.method == 'OPTIONS') {
-	  	ctx.status = 200
-	} else {
-	  await next();
-	}
-});
+  ctx.set('Access-Control-Allow-Origin', ctx.headers.origin || '*')
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With'
+  )
+  ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  ctx.set('Access-Control-Allow-Credentials', true) //可以带cookies
+  ctx.set('X-Powered-By', '3.2.1')
+  if (ctx.method == 'OPTIONS') {
+    ctx.status = 200
+  } else {
+    await next()
+  }
+})
 
 app.keys = ['this is a blog manage system writed by cixiu']
 const SESSION_CONFIG = {
@@ -31,23 +34,39 @@ const SESSION_CONFIG = {
   signed: true
 }
 app.use(session(SESSION_CONFIG, app))
-app.use(koaBody({
-  formLimit: 1024 * 1024,
-  textLimit: 1024 * 1024,
-  multipart: true,
-  formidable: {
-    keepExtensions: true,
-    uploadDir: path.join(__dirname, '../public/images'),
-    onFileBegin: (name, file) => {
-      file.path = path.join(__dirname, '../public/images/' + file.name)
+app.use(
+  koaBody({
+    formLimit: 1024 * 1024,
+    textLimit: 1024 * 1024,
+    multipart: true,
+    formidable: {
+      keepExtensions: true,
+      uploadDir: path.join(__dirname, '../public/images'),
+      onFileBegin: (name, file) => {
+        file.path = path.join(__dirname, '../public/images/' + file.name)
+      }
     }
-  }
-}))
+  })
+)
 
 router(app)
 
-app.listen(port, err => {
-  if (!err) {
-    console.log(`Server is running at ${port}`)
-  }
-})
+if (process.env.NODE_ENV === 'production') {
+  const Static = require('koa-static')
+  const convert = require('koa-convert')
+  const historyApiFallback = require('koa-connect-history-api-fallback')
+  app.use(convert(historyApiFallback()))
+  app.use(Static(path.join(__dirname, '../dist')))
+  const port = 9000
+  app.listen(port, err => {
+    if (!err) {
+      console.log(`Production Server is running at ${port}`)
+    }
+  })
+} else {
+  app.listen(port, err => {
+    if (!err) {
+      console.log(`Development Server is running at ${port}`)
+    }
+  })
+}
